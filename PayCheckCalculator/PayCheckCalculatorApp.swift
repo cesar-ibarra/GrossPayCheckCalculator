b3bba579707a83712f551fceb9156a731f31282a
cesar-ibarra <ibarratcesar@icloud.com>
2022-09-05 17:23:17 -0700

    Initial Commit

---
+++ PayCheckCalculator/PayCheckCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PayCheckCalculatorApp.swift
+//  PayCheckCalculator
+//
+//  Created by Cesar Ibarra on 9/5/22.
+//
+
+import SwiftUI
+
+@main
+struct PayCheckCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
